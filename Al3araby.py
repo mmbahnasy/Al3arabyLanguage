373486482f934a4bdb2ee2551f7d0794822034c9
Mahmoud Bahansy <mmbahnasy@gmail.com>
2026-07-18 18:54:53 -0400

    Updating README file
    Signed-off-by: Mahmoud Bahansy <mmbahnasy@gmail.com>

--- Al3araby.py
+++ Al3araby.py
@@ -5,11 +5,12 @@ from Al3arabyKeywords import Al3arabyKeywords
 variableMap = {}
 
 def main():
-    with open("first.rtl.3araby", "r", encoding="utf-8") as file:
+    with open("first.3araby", "r", encoding="utf-8") as file:
         for line in file:
             # print(f"line: {line}")
             line = line.replace('\xa0', ' ')
             tokens = line.split(" ")
+            print(tokens)
             if tokens:
                 try:
                     index = tokens.index(Al3arabyKeywords["declare"])
