68f9dcc62c5476602a2bc760fbf1f738ddf7886e
Mahmoud Bahnasy <mahmoudbahnasy@Mahmouds-MacBook-Neo.local>
2026-07-18 14:41:13 -0400

    First version

---
+++ Al3araby.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+from Al3arabyKeywords import Al3arabyKeywords
+
+variableMap = {}
+
+def main():
+    with open("first.rtl.3araby", "r", encoding="utf-8") as file:
+        for line in file:
+            # print(f"line: {line}")
+            line = line.replace('\xa0', ' ')
+            tokens = line.split(" ")
+            if tokens:
+                try:
+                    index = tokens.index(Al3arabyKeywords["declare"])
+                    print(f"Tokens: {tokens}")
+                    print(f"Token {Al3arabyKeywords['declare']} was found.")
+                    if tokens[index+2] == "=":
+                        variableMap[tokens[index+1]] = tokens[index+3]
+                        print(f"Variable '{tokens[index+1]}' defined with value '{tokens[index+3]}'.")
+
+                except ValueError:
+                    continue
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ Al3arabyKeywords.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+Al3arabyKeywords = {
+    "declare" : "عرف",
+    "func" : "داله",
+    "print": "إطبع"
+}
